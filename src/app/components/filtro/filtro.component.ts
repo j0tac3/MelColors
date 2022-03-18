4a715f2045d2c99f0e00c4660db31ff872b49328
j0tac3 <j0s3.90jr@gmail.com>
2022-03-19 00:50:44 +0100

    Maquetado el filtro

--- src/app/components/filtro/filtro.component.ts
+++ src/app/components/filtro/filtro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-filtro',
@@ -6,16 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filtro.component.css']
 })
 export class FiltroComponent implements OnInit {
-  public marca : number = 1;
-  public categoria : number = 1;
+  public marcas = [
+    {'id':1,'desc':'Vallejo'},
+    {'id':2,'desc':'Citadel'},
+    {'id':3,'desc':'Army Painter'}
+  ];
+  public categorias = [
+    {'id':0,'desc':'Todos'},
+    {'id':1,'desc':'Model Color'},
+    {'id':2,'desc':'Model Air'},
+    {'id':3,'desc':'Game Color'},
+    {'id':4,'desc':'Game Air'}
+  ];
+  public currentMarca! : number;
+  public currentCategoria! : number;
+  public filtro! : FormGroup;
+  public expanded = false;
 
-  constructor() { }
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.fofmInit();
+    this.currentMarca = this.marcas[0].id;
+    this.currentCategoria = this.categorias[0].id;
+  }
+
+  fofmInit(){    
+    this.filtro = this.fb.group({
+      marca : new FormControl( this.marcas[0].id, Validators.required),
+      categoria : new FormControl(this.categorias[0].id, Validators.required)
+    });
+
   }
 
   onChangeMarca(marca : number){
-    this.marca = marca;
+    this.currentMarca = marca;
+  }
+
+  onChangeCategoria(categoria : number){
+    this.currentCategoria = categoria;
+  }
+
+  clearFilter(){
+    this.filtro.get('marca')?.reset(this.marcas[0].id);
+    this.filtro.get('categoria')?.reset(this.categorias[0].id);
+  }
+
+  onExpand(expand : boolean){
+    this.expanded = expand;
   }
 
 }
